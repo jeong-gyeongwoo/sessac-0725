6d2cb28b5bce02f2ca3b952881a5a58a84f31a9c
gyeong <okbamtol@naver.com>
2023-07-27 18:37:13 +0900

    신조어 사전 과제 추가

--- NewlyCoinedword/Practice/WordViewController.swift
+++ NewlyCoinedword/Practice/WordViewController.swift
@@ -12,6 +12,9 @@ class WordViewController: UIViewController {
     @IBOutlet var resultLabel: UILabel!
     @IBOutlet var wordTextField: UITextField!
     @IBOutlet var wordFirstButton: UIButton!
+    let wordDictionary = ["신조어1":"신조어1입니다","신조어2":"신조어2입니다","신조어3":"신조어3입니다"]
+    let wordList = ["신조어1","신조어2","신조어3"]
+    var wordTextFieldCount: Int = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,27 +45,43 @@ class WordViewController: UIViewController {
     @IBAction func wordButtonTapped(_ sender: UIButton) {
         //print(sender.currentTitle) -> 15이상에서 안나올수도?
         // print(sender.titleLable?.text)
-        wordTextField.text = sender.currentTitle
+        wordTextField.text = wordList[sender.tag - 1]
         textFieldKeyboardTapped(wordTextField)
     }
     
+    
+    
     @IBAction func textFieldKeyboardTapped(_ sender: UITextField) {
         
-        switch wordTextField.text?.uppercased() {
-            // 함수 안이니 sender(파리미터) 사용가능, 영어 대소문자 대응
-        case "신조어3":
-            // 쉼표로 조건을 이을 수 있다 "신조어3","신조어2"
-            resultLabel.text = "신조어 세번째입니다."
-        case "신조어2":
-            // 컬렉션을 써보기
-            resultLabel.text = "신조어 두번째입니다."
-        case "신조어":
-            resultLabel.text = "신조어 첫번째입니다."
-        default:
-            resultLabel.text = "찾는 결과가 없습니다."
+        if let wordTextFieldValue = wordTextField.text {
+            wordTextFieldCount = wordTextFieldValue.count
+        }
+        
+        if wordTextFieldCount >= 2 {
+            
+            switch wordTextField.text?.uppercased() {
+                // 함수 안이니 sender(파리미터) 사용가능, 영어 대소문자 대응
+            case "신조어3":
+                // 쉼표로 조건을 이을 수 있다 "신조어3","신조어2"
+                resultLabel.text = wordDictionary["신조어3"]
+            case "신조어2":
+                // 컬렉션을 써보기
+                resultLabel.text = wordDictionary["신조어2"]
+            case "신조어":
+                resultLabel.text = wordDictionary["신조어1"]
+            default:
+                resultLabel.text = "찾는 결과가 없습니다."
+                
+            }
+        }else {
+            let alert = UIAlertController(title: "알림", message: "한글자이거나 빈칸입니다", preferredStyle: .alert)
+            
+            let button1 = UIAlertAction(title: "확인", style: .default)
+            alert.addAction(button1)
+            present(alert, animated: true)
             
         }
     }
     
-    
 }
+
